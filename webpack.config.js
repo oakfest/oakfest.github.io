5bba7edfdb221c5e3823be4d594ea48dfb20dc8a
John Moore <john.moore@networkts.com>
2018-01-25 15:56:13 -0500

    Added scss watcher

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const MinifyPlugin = require("babel-minify-webpack-plugin");
 const ManifestPlugin = require('webpack-manifest-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const dist = path.resolve(__dirname, 'assets/dist');
 
@@ -12,12 +13,39 @@ const dist = path.resolve(__dirname, 'assets/dist');
 module.exports = function (env) {
     return {
         entry: {
-            'oak.min': './src/js/index.js'
+            'oak.min': './src/js/index.js',
+            'main': './src/css/main.scss'
         },
 
         output: {
             filename: '[name].[chunkhash].js',
-            path: dist
+            path: dist,
+            publicPath: '/assets/dist/'
+        },
+
+        module: {
+
+            rules: [
+                {
+                    test: /\.css$/,
+                    loader: ExtractTextPlugin.extract({
+                        use: 'css-loader?-url'
+                    })
+                },
+                {
+                    test: /\.(sass|scss)$/,
+                    loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+                },
+                {
+                    test: /\.(png|jpg|gif)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {}
+                        }
+                    ]
+                }
+            ]
         },
 
         plugins: [
@@ -28,9 +56,13 @@ module.exports = function (env) {
             new MinifyPlugin({}, {
                 test: /\.min\./,
             }),
+            new ExtractTextPlugin({
+                filename: '[name].bundle.css',
+                allChunks: true,
+            }),
             new HtmlWebpackPlugin({
-              filename: __dirname + '/index.html',
-	      template: __dirname + '/src/index.ejs'
+                filename: __dirname + '/index.html',
+                template: __dirname + '/src/index.ejs'
             })
         ]
     }
