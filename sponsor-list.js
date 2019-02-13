908ac86e9e75ab0f88a71c484858657b6d7d1805
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-13 17:51:12 +0000

    Deploy oakfest/oakfest.github.io to github.com/oakfest/oakfest.github.io.git:master

--- sponsor-list.js
+++ sponsor-list.js
@@ -1,176 +1,180 @@
 module.exports = [
     {
-        level: 'presenting',
-        name: 'Whitehurst, Blackburn & Warren - Attorneys at Law',
-        url: 'http://wbwattorneys.com/',
-        logo: ''
+        level: "presenting",
+        name: "Whitehurst, Blackburn & Warren\nAttorneys at Law",
+        url: "http://wbwattorneys.com/",
+        logo: "wbw.svg"
     },
     {
-        level: 'platinum',
-        name: 'Whelchel & Carlton, LLP',
-        url: 'https://www.wcgalaw.com/',
-        logo: ''
+        level: "platinum",
+        name: "Whelchel & Carlton, LLP",
+        url: "https://www.wcgalaw.com/",
+        logo: "wc.png"
     },
     {
-        level: 'platinum',
-        name: 'CCH Construction',
-        url: 'http://www.cch-construction.com',
-        logo: ''
+        level: "platinum",
+        name: "CCH Construction",
+        url: "http://www.cch-construction.com",
+        logo: "cch.gif"
     },
     {
-        level: 'gold',
-        name: 'Bannister Electric',
-        url: 'https://www.thomasvillechamber.com/list/member/bannister-s-electric-company-inc-thomasville-1150',
-        logo: ''
+        level: "gold",
+        name: "Bannister Electric",
+        url: "https://www.thomasvillechamber.com/list/member/bannister-s-electric-company-inc-thomasville-1150",
+        logo: ""
     },
     {
-        level: 'silver',
-        name: 'Taylor Benefit Resource',
-        url: 'http://www.tbrtpa.com/',
-        logo: ''
+        level: "silver",
+        name: "Taylor Benefit Resource",
+        url: "http://www.tbrtpa.com/",
+        logo: "tbr.svg"
     },
     {
-        level: 'silver',
-        name: 'Wendy\'s',
-        url: 'https://www.wendys.com/',
-        logo: ''
+        level: "silver",
+        name: "Wendy's",
+        url: "https://www.wendys.com/",
+        logo: "wendys.png"
     },
     {
-        level: 'silver',
-        name: 'J. Smith Lanier',
-        url: 'http://www.jsmithlanier.com/',
-        logo: ''
+        level: "silver",
+        name: "J. Smith Lanier & Co.\nA Marsh & McLennan Agency LLC company",
+        url: "http://www.jsmithlanier.com/",
+        logo: "jsmith.jpg"
     },
     {
-        level: 'silver',
-        name: 'CIVES',
-        url: 'http://www.cives.com/southern',
-        logo: ''
+        level: "silver",
+        name: "Cives Steel Company",
+        url: "http://www.cives.com/southern",
+        logo: "cives.png"
     },
     {
-        level: 'silver',
-        name: 'Cleaver Brooks',
-        url: 'http://www.cleaverbrooks.com/',
-        logo: ''
+        level: "silver",
+        name: "Cleaver Brooks\nComplete Boiler Room Solutions",
+        url: "http://www.cleaverbrooks.com/",
+        logo: "cleaver.png"
     },
     {
-        level: 'silver',
-        name: 'Factor X Fitness',
-        url: 'https://www.factorxfitness.com/',
-        logo: ''
+        level: "silver",
+        name: "Factor X Fitness",
+        url: "https://www.factorxfitness.com/",
+        logo: "factor.png"
     },
     {
-        level: 'silver',
-        name: 'Southern Regional Technical College',
-        url: 'https://southernregional.edu/',
-        logo: ''
+        level: "silver",
+        name: "Southern Regional Technical College",
+        url: "https://southernregional.edu/",
+        logo: "srtc.png"
     },
     {
-        level: 'silver',
-        name: 'CNS',
-        url: 'https://www.cns-internet.com',
-        logo: ''
+        level: "silver",
+        name: "CNS\nInternet - Phone - Digital TV",
+        url: "https://www.cns-internet.com",
+        logo: "cns.png"
     },
     {
-        level: 'silver',
-        name: 'City of Thomasville',
-        url: 'https://thomasville.org',
-        logo: ''
+        level: "silver",
+        name: "City of Thomasville",
+        url: "https://thomasville.org",
+        logo: "thomasville.png"
     },
     {
-        level: 'bronze',
-        name: 'Element Style Bar',
-        url: 'https://www.facebook.com/laurendeeabbey/',
-        logo: ''
+        level: "bronze",
+        name: "Element Style Bar",
+        url: "https://www.facebook.com/laurendeeabbey/",
+        logo: "element.png"
     },
     {
-        level: 'bronze',
-        name: 'Liberty Towing',
-        url: 'https://www.facebook.com/libertytowingandrecovery/',
-        logo: ''
+        level: "bronze",
+        name: "Liberty Towing",
+        url: "https://www.facebook.com/libertytowingandrecovery/",
+        logo: "liberty.png",
+        tiny: true
     },
     {
-        level: 'bronze',
-        name: 'Ameris',
-        url: 'https://www.amerisbank.com/',
-        logo: ''
+        level: "bronze",
+        name: "Ameris Bank",
+        url: "https://www.amerisbank.com/",
+        logo: "ameris.png"
     },
     {
-        level: 'bronze',
-        name: 'Synovus',
-        url: 'https://www.bankcb.com/',
-        logo: ''
+        level: "bronze",
+        name: "Synovus Bank",
+        url: "https://synovus.com",
+        logo: "synovus.png"
     },
     {
-        level: 'bronze',
-        name: 'Danny Hayes',
-        url: 'http://dannyhayesmylawyer.com/',
-        logo: ''
+        level: "bronze",
+        name: "Danny Hayes\nAttorney at Law",
+        url: "http://dannyhayesmylawyer.com/",
+        logo: "danny.png"
     },
     {
-        level: 'bronze',
-        name: 'Caldwell & Langford',
-        url: 'http://caldwellandlangford.com/',
-        logo: ''
+        level: "bronze",
+        name: "Caldwell & Langford Insurance",
+        url: "http://caldwellandlangford.com/",
+        logo: "caldwell.svg"
     },
     {
-        level: 'bronze',
-        name: 'McIntosh Clinic',
-        url: 'http://www.mcintoshclinic.com/',
-        logo: ''
+        level: "bronze",
+        name: "McIntosh Clinic",
+        url: "http://www.mcintoshclinic.com/",
+        logo: "mcintosh.jpg"
     },
     {
-        level: 'bronze',
-        name: 'Henderson\'s',
-        url: 'http://hendersonsofthomasville.com/',
-        logo: ''
+        level: "bronze",
+        name: "Henderson's Restaurant",
+        url: "http://hendersonsofthomasville.com/",
+        logo: "hendersons.svg"
     },
     {
-        level: 'bronze',
-        name: 'Sampson\'s',
-        url: 'http://paintstore.benjaminmoore.com/stores/us/ga/thomasville/sampsons-paint-decorating-inc?lang=en_CA',
-        logo: ''
+        level: "bronze",
+        name: "Sampson's Paint and Decorating",
+        url: "http://paintstore.benjaminmoore.com/stores/us/ga/thomasville/sampsons-paint-decorating-inc?lang=en_CA",
+        logo: "sampsons.png",
+        tiny: true
     },
     {
-        level: 'bronze',
-        name: 'State Farm - Patrick Scarborough',
-        url: 'https://www.tvilleinsurance.com/',
-        logo: ''
+        level: "bronze",
+        name: "State Farm\nPatrick Scarborough",
+        url: "https://www.tvilleinsurance.com/",
+        logo: "state-farm.svg"
     },
     {
-        level: 'bronze',
-        name: 'AgPro',
-        url: 'https://www.agprocompanies.com/',
-        logo: ''
+        level: "bronze",
+        name: "AgPro Companies",
+        url: "https://www.agprocompanies.com/",
+        logo: "agpro.png",
+        tiny: true
     },
     {
-        level: 'bronze',
-        name: 'Terramore Development',
-        url: 'http://www.teramore.net/',
-        logo: ''
+        level: "bronze",
+        name: "Teramore Development",
+        url: "http://www.teramore.net/",
+        logo: "teramore.png",
+        tiny: true
     },
     {
-        level: 'bronze',
-        name: 'Nutrien Ag Solutions',
-        url: 'https://www.nutrienagsolutions.com/',
-        logo: ''
+        level: "bronze",
+        name: "Nutrien Ag Solutions",
+        url: "https://www.nutrienagsolutions.com/",
+        logo: "nutrien.png"
     },
     {
-        level: 'bronze',
-        name: 'Lanigan & Associates P.C.',
-        url: 'https://lanigancpa.com/',
-        logo: ''
+        level: "bronze",
+        name: "Lanigan & Associates P.C.",
+        url: "https://lanigancpa.com/",
+        logo: "lanigan.svg"
     },
     {
-        level: 'bronze',
-        name: 'New Hire Solutions',
-        url: 'http://newhiresolutions.com/',
-        logo: ''
+        level: "bronze",
+        name: "New Hire Solutions",
+        url: "http://newhiresolutions.com/",
+        logo: "new-hire.png"
     },
     {
-        level: 'bronze',
-        name: 'The Avenues',
-        url: 'http://theavenuesrealestate.com/',
-        logo: ''
+        level: "bronze",
+        name: "The Avenues\nReal Estate Partners",
+        url: "http://theavenuesrealestate.com/",
+        logo: "avenues.png"
     }
 ];
