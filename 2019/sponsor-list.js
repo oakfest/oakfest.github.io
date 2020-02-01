b7cefe0e973356c2d41d557cd8f24d1edc4a5840
John Moore <johndudleymoore@gmail.com>
2020-02-01 17:35:42 -0500

    Archive 2019 site

---
+++ 2019/sponsor-list.js
@@ -0,0 +1,259 @@
+module.exports = [
+    {
+        level: "presenting",
+        name: "Whitehurst, Blackburn & Warren\nAttorneys at Law",
+        url: "http://wbwattorneys.com/",
+        logo: "wbw.svg"
+    },
+    {
+        level: "platinum",
+        name: "Whelchel & Carlton, LLP",
+        url: "https://www.wcgalaw.com/",
+        logo: "wc.png"
+    },
+    {
+        level: "platinum",
+        name: "CCH Construction",
+        url: "http://www.cch-construction.com",
+        logo: "cch.gif"
+    },
+    {
+        level: "gold",
+        name: "Bannister Electric",
+        url: "https://www.thomasvillechamber.com/list/member/bannister-s-electric-company-inc-thomasville-1150",
+        logo: ""
+    },
+    {
+        level: "silver",
+        name: "Taylor Benefit Resource",
+        url: "http://www.tbrtpa.com/",
+        logo: "tbr.svg"
+    },
+    {
+        level: "silver",
+        name: "Wendy's",
+        url: "https://www.wendys.com/",
+        logo: "wendys.png"
+    },
+    {
+        level: "silver",
+        name: "J. Smith Lanier & Co.\nA Marsh & McLennan Agency LLC company",
+        url: "http://www.jsmithlanier.com/",
+        logo: "jsmith.jpg"
+    },
+    {
+        level: "silver",
+        name: "Cives Steel Company",
+        url: "http://www.cives.com/southern",
+        logo: "cives.png"
+    },
+    {
+        level: "silver",
+        name: "Cleaver Brooks\nComplete Boiler Room Solutions",
+        url: "http://www.cleaverbrooks.com/",
+        logo: "cleaver.png"
+    },
+    {
+        level: "silver",
+        name: "Factor X Fitness",
+        url: "https://www.factorxfitness.com/",
+        logo: "factor.png"
+    },
+    {
+        level: "silver",
+        name: "Southern Regional Technical College",
+        url: "https://southernregional.edu/",
+        logo: "srtc.png"
+    },
+    {
+        level: "silver",
+        name: "CNS\nInternet - Phone - Digital TV",
+        url: "https://www.cns-internet.com",
+        logo: "cns.png"
+    },
+    {
+        level: "silver",
+        name: "Long & Company CPA's LLC",
+        url: "https://www.longcocpa.com/",
+        logo: ""
+    },
+    {
+        level: "silver",
+        name: "Allgood Screen Printing & Embroidery",
+        url: "https://www.companycasuals.com/allgood/start.jsp",
+        logo: "allgood.png",
+        tiny: true
+    },
+    {
+        level: "silver",
+        name: "Southern Elder Law Center",
+        url: "http://southernelderlaw.com/",
+        logo: "elder.png"
+    },
+    {
+        level: "bronze",
+        name: "Element Style Bar",
+        url: "https://www.facebook.com/laurendeeabbey/",
+        logo: "element.png"
+    },
+    {
+        level: "bronze",
+        name: "Liberty Towing",
+        url: "https://www.facebook.com/libertytowingandrecovery/",
+        logo: "liberty.png",
+        tiny: true
+    },
+    {
+        level: "bronze",
+        name: "Ameris Bank",
+        url: "https://www.amerisbank.com/",
+        logo: "ameris.png"
+    },
+    {
+        level: "bronze",
+        name: "Synovus Bank",
+        url: "https://synovus.com",
+        logo: "synovus.png"
+    },
+    {
+        level: "bronze",
+        name: "Danny Hayes\nAttorney at Law",
+        url: "http://dannyhayesmylawyer.com/",
+        logo: "danny.png"
+    },
+    {
+        level: "bronze",
+        name: "Caldwell & Langford Insurance",
+        url: "http://caldwellandlangford.com/",
+        logo: "caldwell.svg"
+    },
+    {
+        level: "bronze",
+        name: "McIntosh Clinic",
+        url: "http://www.mcintoshclinic.com/",
+        logo: "mcintosh.jpg"
+    },
+    {
+        level: "bronze",
+        name: "Henderson's Restaurant",
+        url: "http://hendersonsofthomasville.com/",
+        logo: "hendersons.svg"
+    },
+    {
+        level: "bronze",
+        name: "Sampson's Paint and Decorating",
+        url: "http://paintstore.benjaminmoore.com/stores/us/ga/thomasville/sampsons-paint-decorating-inc?lang=en_CA",
+        logo: "sampsons.png",
+        tiny: true
+    },
+    {
+        level: "bronze",
+        name: "State Farm\nPatrick Scarborough",
+        url: "https://www.tvilleinsurance.com/",
+        logo: "state-farm.svg"
+    },
+    {
+        level: "bronze",
+        name: "AgPro Companies",
+        url: "https://www.agprocompanies.com/",
+        logo: "agpro.png",
+        tiny: true
+    },
+    {
+        level: "bronze",
+        name: "Teramore Development",
+        url: "http://www.teramore.net/",
+        logo: "teramore.png",
+        tiny: true
+    },
+    {
+        level: "bronze",
+        name: "Nutrien Ag Solutions",
+        url: "https://www.nutrienagsolutions.com/",
+        logo: "nutrien.png"
+    },
+    {
+        level: "bronze",
+        name: "Lanigan & Associates P.C.",
+        url: "https://lanigancpa.com/",
+        logo: "lanigan.svg"
+    },
+    {
+        level: "bronze",
+        name: "New Hire Solutions",
+        url: "http://newhiresolutions.com/",
+        logo: "new-hire.png"
+    },
+    {
+        level: "bronze",
+        name: "The Avenues\nReal Estate Partners",
+        url: "http://theavenuesrealestate.com/",
+        logo: "avenues.png"
+    },
+    {
+        level: "bronze",
+        name: "Potty Man\nPortable Toilet Rentals",
+        url: "https://pottyman.net/",
+        logo: "pottyman.png"
+    },
+    {
+        level: "bronze",
+        name: "TC Federal Bank",
+        url: "https://www.tcfederal.com/",
+        logo: "tcf.png"
+    },
+    {
+        level: "bronze",
+        name: "Thomasville National Bank\nTNB Financial",
+        url: "https://tnbank.com/",
+        logo: "tnbboth.png"
+    },
+    {
+        level: "bronze",
+        name: "Allen & Allen\nFuneral Home and Crematory",
+        url: "https://www.allenfh.com/",
+        logo: "aa.webp"
+    },
+    {
+        level: "bronze",
+        name: "Collins Pecan",
+        url: "https://www.collinspecan.com/",
+        logo: "collins.png"
+    },
+    {
+        level: "bronze",
+        name: "Performance Food Group",
+        url: "http://www.pfgc.com/",
+        logo: "pfg.png"
+    },
+    {
+        level: "bronze",
+        name: "Joey's Landscape and Lawn Care Services,\nLLC.",
+        url: "http://www.joeyslandscape.com/",
+        logo: "jlls.png"
+    },
+    {
+        level: "bronze",
+        name: "Network Technology Solutions",
+        url: "https://ntsnetworks.com",
+        logo: "nts.svg"
+    },
+    {
+        level: "bronze",
+        name: "Three Toms Tavern",
+        url: "#",
+        logo: ""
+    },
+    {
+        level: "bronze",
+        name: "The Orr Family",
+        url: "#",
+        logo: ""
+    },
+    {
+        level: "bronze",
+        name: "Ashlyn Williams",
+        url: "#",
+        logo: ""
+    }
+];
