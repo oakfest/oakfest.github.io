21930f6e638e000ed01552d512a6732bcbbc86f2
John Moore <johndudleymoore@gmail.com>
2020-02-02 13:18:22 -0500

    Fix mobile stylings for countdown timer.

---
+++ src/js/countdown.js
@@ -0,0 +1,29 @@
+export default function (endDate, parentId, daysId, hoursId, minutesId, secondsId) {
+    const count = document.getElementById(parentId);
+
+    count.style.display = 'block';
+
+    if (count) {
+        const daysEl = document.getElementById(daysId);
+        const hoursEl = document.getElementById(hoursId);
+        const minutesEl = document.getElementById(minutesId);
+        const secondsEl = document.getElementById(secondsId);
+
+        const updateCounter = () => {
+            const diff = endDate - Date.now();
+            const days = Math.floor(diff / (24 * 3600 * 1000));
+            const hours = Math.floor((diff - (days * (24 * 3600 * 1000))) / (3600 * 1000));
+            const minutes = Math.floor((diff - (hours * (3600 * 1000)) - (days * (24 * 3600 * 1000))) / (60 * 1000));
+            const seconds = Math.floor((diff - (hours * (3600 * 1000)) - (days * (24 * 3600 * 1000)) - (minutes * (60 * 1000))) / (1000));
+
+            daysEl.innerText = days + "";
+            hoursEl.innerText = hours + "";
+            minutesEl.innerText = minutes + "";
+            secondsEl.innerText = seconds + "";
+        };
+
+        updateCounter();
+
+        setInterval(updateCounter, 1000);
+    }
+}
